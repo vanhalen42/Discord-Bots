744134a7fe5d8c027d28a4b07d92ce9c67396f4c
Gaurav Singh <71246220+vanhalen42@users.noreply.github.com>
2021-10-20 14:58:56 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,44 @@
+# import the discord library
+
+import discord
+from discord import message
+from discord import embeds
+import os
+
+# bot connect
+client = discord.Client()
+# this variable stores id of an output channel, you can have many variables corresponding to different channels
+out_channel = '' // add default out_channel
+
+motor_functions = [0]
+
+
+@client.event
+# new event
+async def on_ready():
+    # output_channel object holds the info of that channel, whos id is provided
+    output_channel = client.get_channel(out_channel)
+    # code to send message is
+    await output_channel.send("I art been summoneth.")
+
+
+@client.event
+async def on_message(message):
+    output_channel = message.channel
+    if message.content == "too much F now" and message.author.id == 0:
+        motor_functions[0] = 1
+    if message.content == "continue rr" and message.author.id == 0:
+        motor_functions[0] = 0
+    input_mssg = message.content  # message.content is the string of that message
+    if input_mssg[0] == 'F' and motor_functions[0] == 0:
+        mssg = input_mssg.split(" ", 1)
+        command = mssg[0]
+        if command=='F' or command == 'f':
+            str = mssg[1]*5 + "\n" + mssg[1] + "\n" + mssg[1] + "\n" + mssg[1]*4 + \
+                "\n" + mssg[1] + "\n" + mssg[1] + \
+                "\n" + mssg[1] + "\n" + mssg[1]
+            # print(str)
+            await output_channel.send(str)
+
+# Run the client on this server
+client.run('bot_token') // add a bot token
