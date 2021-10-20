b60e5ef013a730405bf75f4747378318b53bc554
Gaurav Singh <gauravsinghk03@gmail.com>
2021-10-20 15:25:01 +0530

    Fbot done

--- Fbot/bot.py
+++ Fbot/bot.py
@@ -4,11 +4,13 @@ import discord
 from discord import message
 from discord import embeds
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # bot connect
 client = discord.Client()
 # this variable stores id of an output channel, you can have many variables corresponding to different channels
-out_channel = '' // add default out_channel
+out_channel = int(os.getenv("OUT_CHANNEL")) # add default out_channel
 
 motor_functions = [0]
 
@@ -25,9 +27,9 @@ async def on_ready():
 @client.event
 async def on_message(message):
     output_channel = message.channel
-    if message.content == "too much F now" and message.author.id == 0:
+    if message.content == "too much F now" and message.author.id == int(os.getenv("ADMIN")):
         motor_functions[0] = 1
-    if message.content == "continue rr" and message.author.id == 0:
+    if message.content == "continue rr" and message.author.id == int(os.getenv("ADMIN")):
         motor_functions[0] = 0
     input_mssg = message.content  # message.content is the string of that message
     if input_mssg[0] == 'F' and motor_functions[0] == 0:
@@ -41,4 +43,4 @@ async def on_message(message):
             await output_channel.send(str)
 
 # Run the client on this server
-client.run('bot_token') // add a bot token
+client.run(os.getenv('BOT_TOKEN')) # add a bot token
